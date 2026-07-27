d73e289180024104761fda4e976a236e71dbfa28
Manish zond <manishzond07@gmail.com>
2026-07-27 15:33:48 +0530

    new lesson drop useEffect

--- 2Basic-of-ReactTypscript/1myapp/hooks/UseEffect.tsx
+++ 2Basic-of-ReactTypscript/1myapp/hooks/UseEffect.tsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from "react";
+
+type UserInfoType = {
+  id: number;
+  username: string;
+  firstName: string;
+  lastName: string;
+  age: number;
+  email: string;
+};
+
+type ApiResponse = {
+  users: UserInfoType[];
+  total: number;
+  skip: number;
+  limit: number;
+};
+
 function UseEffect() {
+  const [users, setUsers] = useState<UserInfoType[] | null>(null);
+
+  useEffect(() => {
+    let isCancelled = false;
+
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      .then((result: ApiResponse) => {
+        if (!isCancelled) setUsers(result.users);
+      });
+
+    return () => {
+      isCancelled = true;
+    };
+  }, []);
+
   return (
     <>
       <h1>Use Effect</h1>
+      {users?.map((user) => (
+        <div key={user.id}>
+          <p>{user.username}</p>
+          <p>{user.firstName}</p>
+          <p>{user.lastName}</p>
+          <p>{user.age}</p>
+          <p>{user.email}</p>
+        </div>
+      ))}
     </>
   );
 }
